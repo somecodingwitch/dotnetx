8b86eea1283b3c41d3dd8ea3e7d3b02d3c8592e9
Scarlet <70824102+somecodingwitch@users.noreply.github.com>
2024-06-16 21:48:40 -0300

    weird wip about csproj probably remaking everything soon

--- src/commands/csproj.ts
+++ src/commands/csproj.ts
@@ -1,10 +1,45 @@
 import { readFile } from "fs/promises";
 import { toJson } from 'xml2json'
-async function csproj(flags: string[]) {
 
+async function csproj(args: string[]) {
+    const path = args[args.indexOf('--path') + 1];
+    const data = await _getCsprojData(path);
+    console.log(data)
 }
 
 async function _getCsprojData(path: string) {
     const xmlContent = (await readFile(path)).toString();
-    const jsonContent = JSON.parse(toJson(xmlContent));
+    const jsonContent = JSON.parse(toJson(xmlContent)).Project;
+    
+    const propertyGroup = _flatAndDecapitalizeObjects(jsonContent.PropertyGroup)
+    const itemGroup = _flatAndDecapitalizeObjects(jsonContent.ItemGroup);
+
+    const metadata = propertyGroup;
+    const references = _flatPackageReferences(itemGroup.packageReference);
+    
+    return {
+        metadata,
+        references
+    }
 }
+
+function _flatAndDecapitalizeObjects(obj: any[]) {
+    const flat = obj.reduce((acc, cur) => ({ ...cur, ...acc }));
+    const x = {} as any;
+
+    for (const [key, value] of Object.entries(flat)) {
+        const newKey = key[0].toLowerCase() + key.slice(1);
+        x[newKey] = value;
+    }
+
+    return x;
+}
+
+function _flatPackageReferences(obj: any[]) {
+    return obj.reduce((acc, curr) => {
+        return { [curr.Include]: curr.Version, ...acc }
+    });
+}
+
+
+export { csproj }
--- src/commands/stored-commands.ts
+++ src/commands/stored-commands.ts
@@ -1,10 +1,12 @@
 import { checkForDotnet } from "../command-line/check-for-dotnet.js";
+import { csproj } from "./csproj.js";
 import { versions } from "./versions.js";
 
 const storedCommands = {
     'status': () => console.log(checkForDotnet(false)),
     'versions': (args: string[]) => versions(args),
-    'install': (args: string[]) => "todo"
+    'install': (args: string[]) => "todo",
+    'csproj': (args: string[]) => csproj(args)
 };
 
 export { storedCommands }
